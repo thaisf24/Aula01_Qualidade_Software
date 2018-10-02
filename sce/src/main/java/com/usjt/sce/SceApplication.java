8c377e54d6da55e3595c10ee99426f8804b06b73
DV6-6170\Hp <engsoftware1@gmail.com>
2018-10-02 11:39:17 -0300

    versao inicial - usjt

---
+++ sce/src/main/java/com/usjt/sce/SceApplication.java
@@ -0,0 +1,12 @@
+package com.usjt.sce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SceApplication.class, args);
+	}
+}
