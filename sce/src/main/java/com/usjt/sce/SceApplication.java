129b37223ddad36721468ae29431a88bb7c4953e
DV6-6170\Hp <engsoftware1@gmail.com>
2018-10-03 14:04:09 -0300

    Configuração no pom do log4j e da api joda time

--- sce/src/main/java/com/usjt/sce/SceApplication.java
+++ sce/src/main/java/com/usjt/sce/SceApplication.java
@@ -1,5 +1,6 @@
 package com.usjt.sce;
 
+import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SceApplication {
 
 	public static void main(String[] args) {
+		Logger logger = Logger.getLogger(SceApplication.class);
 		SpringApplication.run(SceApplication.class, args);
+		logger.info("aplicação executada com sucesso");
 	}
 }
